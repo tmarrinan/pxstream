7579066942e3e8a836cf9aa791525ea5516b5d7a
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2018-07-13 10:27:56 -0500

    working streaming

--- src/client/pxclient.cpp
+++ src/client/pxclient.cpp
@@ -1,16 +1,41 @@
 #include <iostream>
+#include <vector>
+#include <numeric>
 #include <netsocket/client.h>
 #include <mpi.h>
 #include <ddr.h>
-#include <glad/glad.h>
+#include <glad/glad.h>]
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
+#include <glm/mat4x4.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 typedef struct Screen {
     int width;
     int height;
     char title[96];
 } Screen;
+typedef struct GShaderProgram {
+    GLuint program;
+    GLint proj_uniform;
+    GLint mv_uniform;
+    GLint img_uniform;
+} GShaderProgram;
+
+void Init(GLFWwindow *window, Screen &screen, GShaderProgram *shader, GLuint *vao, GLuint *tex_id);
+void Render(GLFWwindow *window, GShaderProgram& shader, GLuint vao, GLuint tex_id);
+GLuint CreateRectangleVao();
+GShaderProgram CreateTextureShader();
+GLint CompileShader(char *source, uint32_t length, GLint type);
+void CreateShaderProgram(GLint vertex_shader, GLint fragment_shader, GLuint *program);
+void LinkShaderProgram(GLuint program);
+int32_t ReadFile(const char* filename, char** data_ptr);
+
+GLuint vertex_position_attrib = 0;
+GLuint vertex_texcoord_attrib = 1;
+glm::mat4 mat_projection;
+glm::mat4 mat_modelview;
 
 int main(int argc, char **argv)
 {
@@ -23,6 +48,114 @@ int main(int argc, char **argv)
     {
         fprintf(stderr, "Error initializing MPI and obtaining task ID information\n");
         MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    // NetSocket clients
+    if (argc < 3)
+    {
+        fprintf(stderr, "Error: no host and port provided for server (rank 0)\n");
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+    int i;
+    std::vector<NetSocket::Client*> clients;
+    NetSocket::ClientOptions options = NetSocket::CreateClientOptions();
+    options.secure = false;
+    options.flags = NetSocket::GeneralFlags::None;
+    int num_remote_ranks;
+    uint8_t *remote_ip_addresses;
+    uint16_t *remote_ports;
+    if (rank == 0)
+    {
+        NetSocket::Client *c = new NetSocket::Client(argv[1], atoi(argv[2]), options);
+        clients.push_back(c);
+        int received_server_info = 0;
+        while (received_server_info < 2)
+        {
+            NetSocket::Client::Event event = c->WaitForNextEvent();
+            switch (event.type)
+            {
+                case NetSocket::Client::EventType::ReceiveBinary:
+                    if (received_server_info == 0) // ip addresses
+                    {
+                        num_remote_ranks = event.data_length / 4;
+                        remote_ip_addresses = (uint8_t*)event.binary_data;
+                        received_server_info++;
+                    }
+                    else                           // ports
+                    {
+                        remote_ports = (uint16_t*)event.binary_data;
+                        for (i = 0; i<num_remote_ranks; i++)
+                        {
+                            remote_ports[i] = ntohs(remote_ports[i]);
+                        }
+                        received_server_info++;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    MPI_Bcast(&num_remote_ranks, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    if (rank != 0)
+    {
+        remote_ip_addresses = new uint8_t[4 * num_remote_ranks];
+        remote_ports = new uint16_t[num_remote_ranks];
+    }
+    MPI_Bcast(remote_ip_addresses, 4 * num_remote_ranks, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+    MPI_Bcast(remote_ports, num_remote_ranks, MPI_UNSIGNED_SHORT, 0, MPI_COMM_WORLD);
+    if (rank == 0)
+    {
+        uint8_t info_received[9];
+        uint32_t *info_remote_ranks = (uint32_t*)(info_received + 1);
+        uint32_t *info_ranks = (uint32_t*)(info_received + 5);
+        info_received[0] = 1;
+        *info_remote_ranks = htonl(num_remote_ranks);
+        *info_ranks = htonl(num_ranks);
+        clients[0]->Send(info_received, 9, NetSocket::CopyMode::MemCopy);
+        if (clients[0]->WaitForNextEvent().type != NetSocket::Client::EventType::SendFinished)
+        {
+            printf("Error: unexpected event\n");
+        }
+    }
+    int connections_per_rank = num_remote_ranks / num_ranks;
+    int connections_extra = num_remote_ranks % num_ranks;
+    int num_connections = connections_per_rank + (rank < connections_extra ? 1 : 0);
+    int connection_offset = rank * connections_per_rank + std::min(rank, connections_extra);
+    int *connection_widths = new int[num_connections];
+    int *connection_heights = new int[num_connections];
+    int *connection_offsets = new int[num_connections];
+    int offset = 0;
+    for (i = connection_offset; i < connection_offset + num_connections; i++)
+    {
+        NetSocket::Client *c;
+        if (i > 0)
+        {
+            struct in_addr addr = {*((in_addr_t*)(&(remote_ip_addresses[4*i])))};
+            c = new NetSocket::Client(inet_ntoa(addr), remote_ports[i], options);
+            NetSocket::Client::Event event;
+            do
+            {
+                event = c->WaitForNextEvent();
+            } while (event.type != NetSocket::Client::EventType::Connect);
+            clients.push_back(c);
+        }
+        else
+        {
+            c = clients[0];
+        }
+        NetSocket::Client::Event event = c->WaitForNextEvent();
+        if (event.type == NetSocket::Client::EventType::ReceiveBinary && event.data_length == 8)
+        {
+            connection_widths[i - connection_offset] = ntohl(*((uint32_t*)event.binary_data));
+            connection_heights[i - connection_offset] = ntohl(*((uint32_t*)event.binary_data + 1));
+            connection_offsets[i - connection_offset] = offset;
+            offset += connection_widths[i - connection_offset] * connection_heights[i - connection_offset] * 4;
+            printf("[rank %d] client %d: %dx%d\n", rank, i, connection_widths[i - connection_offset], connection_heights[i - connection_offset]);
+        }
+        else {
+            printf("[rank %d] client %d: unexpected event %d\n", rank, i, event.type);
+        }
     }
 
     // initialize GLFW
@@ -46,6 +179,7 @@ int main(int argc, char **argv)
 
     // make window's context current
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
     // initialize Glad OpenGL extension handling
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -54,9 +188,70 @@ int main(int argc, char **argv)
     }
 
     // main event loop
+    GShaderProgram shader;
+    GLuint vao;
+    GLuint tex_id;
+    Init(window, screen, &shader, &vao, &tex_id);
+
+    bool *receive_data = new bool[num_connections];
+    int tex_width = connection_widths[0];
+    int tex_height = std::accumulate(connection_heights, connection_heights + num_connections, 0);
+    uint8_t *texture = new uint8_t[tex_width * tex_height * 4];
+    uint8_t complete = 1;
+    int it = 0;
     while (!glfwWindowShouldClose(window))
     {
-        glfwWaitEvents();
+        glfwPollEvents();
+        
+        for (i = 0; i < num_connections; i++)
+        {
+            receive_data[i] = false;
+        }
+        bool all_received = false;
+        printf("[rank %d] starting iteration %d\n", rank, it);
+        while (!all_received)
+        {
+            for (i = 0; i < clients.size(); i++)
+            {
+                if (receive_data[i] == false)
+                {
+                    NetSocket::Client::Event event = clients[i]->PollForNextEvent();
+                    if (event.type == NetSocket::Client::EventType::ReceiveBinary)
+                    {
+                        memcpy(texture + connection_offsets[i], event.binary_data, event.data_length);
+                        delete[] event.binary_data;
+                        receive_data[i] = true;
+                    }
+                }
+            }
+            all_received = true;
+            for (i = 0; i < num_connections; i++)
+            {
+                all_received &= receive_data[i];
+            }
+        }
+
+        for (i = 0; i < clients.size(); i++)
+        {
+            clients[i]->Send(&complete, 1, NetSocket::CopyMode::ZeroCopy);
+        }
+        
+        glBindTexture(GL_TEXTURE_2D, tex_id);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tex_width, tex_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture);
+        glBindTexture(GL_TEXTURE_2D, 0);
+        Render(window, shader, vao, tex_id);
+        printf("[rank %d] showing iteration %d\n", rank, it);
+
+        for (i = 0; i < clients.size(); i++)
+        {
+            NetSocket::Client::Event event;
+            do
+            {
+                event = clients[i]->PollForNextEvent();
+            } while (event.type != NetSocket::Client::EventType::SendFinished);
+        }
+        printf("[rank %d] finishing iteration %d\n", rank, it);
+        it++;
     }
 
     // finalize
@@ -65,4 +260,200 @@ int main(int argc, char **argv)
     MPI_Finalize();
 
     return 0;
+}
+
+void Init(GLFWwindow *window, Screen &screen, GShaderProgram *shader, GLuint *vao, GLuint *tex_id)
+{
+    glClearColor(0.0, 0.0, 1.0, 1.0);
+    glEnable(GL_DEPTH_TEST);
+    glViewport(0, 0, screen.width, screen.height);
+    printf("OpenGL: %s\n", glGetString(GL_VERSION));
+
+    *shader = CreateTextureShader();
+    *vao = CreateRectangleVao();
+
+    GLubyte blank[4] = {255, 255, 0, 255};
+    glGenTextures(1, tex_id);
+    glBindTexture(GL_TEXTURE_2D, *tex_id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1, 1, 0, GL_RGBA, GL_UNSIGNED_BYTE, blank);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    mat_projection = glm::ortho(0.0, (double)screen.width, (double)screen.height, 0.0, 1.0, -1.0);
+    mat_modelview = glm::scale(glm::mat4(1.0), glm::vec3(screen.width, screen.height, 1.0));
+
+    Render(window, *shader, *vao, *tex_id);
+}
+
+void Render(GLFWwindow *window, GShaderProgram& shader, GLuint vao, GLuint tex_id)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glUseProgram(shader.program);
+    glUniformMatrix4fv(shader.proj_uniform, 1, GL_FALSE, glm::value_ptr(mat_projection));
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(vao);
+    glUniformMatrix4fv(shader.mv_uniform, 1, GL_FALSE, glm::value_ptr(mat_modelview));
+    glBindTexture(GL_TEXTURE_2D, tex_id);
+    glUniform1i(shader.img_uniform, 0);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    glBindVertexArray(0);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    glfwSwapBuffers(window);
+}
+
+GLuint CreateRectangleVao()
+{
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // vertices
+    GLuint vertex_position_buffer;
+    glGenBuffers(1, &vertex_position_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_position_buffer);
+    GLfloat vertices[12] = {
+        0.0, 0.0, 0.0,
+        0.0, 1.0, 0.0,
+        1.0, 0.0, 0.0,
+        1.0, 1.0, 0.0
+    };
+    glBufferData(GL_ARRAY_BUFFER, 12 * sizeof(GLfloat), vertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(vertex_position_attrib);
+    glVertexAttribPointer(vertex_position_attrib, 3, GL_FLOAT, false, 0, 0);
+
+    // textures
+    GLuint vertex_texcoord_buffer;
+    glGenBuffers(1, &vertex_texcoord_buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_texcoord_buffer);
+    GLfloat texcoords[8] = {
+        0.0, 0.0,
+        0.0, 1.0,
+        1.0, 0.0,
+        1.0, 1.0
+    };
+    glBufferData(GL_ARRAY_BUFFER, 8 * sizeof(GLfloat), texcoords, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(vertex_texcoord_attrib);
+    glVertexAttribPointer(vertex_texcoord_attrib, 2, GL_FLOAT, false, 0, 0);
+
+    // faces of the triangles
+    GLuint vertex_index_buffer;
+    glGenBuffers(1, &vertex_index_buffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vertex_index_buffer);
+    GLushort indices[6] = {
+        0, 3, 1,
+        3, 0, 2
+    };
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(GLushort), indices, GL_STATIC_DRAW);
+
+    glBindVertexArray(0);
+
+    return vao;
+}
+
+GShaderProgram CreateTextureShader()
+{
+    GShaderProgram shader;
+
+    const char *vertex_file = "resrc/shaders/texture.vert";
+    char *vertex_src;
+    int32_t vertex_src_length = ReadFile(vertex_file, &vertex_src);
+    GLint vertex_shader = CompileShader(vertex_src, vertex_src_length, GL_VERTEX_SHADER);
+    free(vertex_src);
+
+    const char *fragment_file = "resrc/shaders/texture.frag";
+    char *fragment_src;
+    int32_t fragment_src_length = ReadFile(fragment_file, &fragment_src);
+    GLint fragment_shader = CompileShader(fragment_src, fragment_src_length, GL_FRAGMENT_SHADER);
+    free(fragment_src);
+
+    CreateShaderProgram(vertex_shader, fragment_shader, &shader.program);
+
+    glBindAttribLocation(shader.program, vertex_position_attrib, "aVertexPosition");
+    glBindAttribLocation(shader.program, vertex_texcoord_attrib, "aVertexTexCoord");
+    glBindAttribLocation(shader.program, 0, "FragColor");
+
+    LinkShaderProgram(shader.program);
+
+    shader.mv_uniform = glGetUniformLocation(shader.program, "uModelViewMatrix");
+    shader.proj_uniform = glGetUniformLocation(shader.program, "uProjectionMatrix");
+    shader.img_uniform = glGetUniformLocation(shader.program, "uImage");
+
+    return shader;
+}
+
+GLint CompileShader(char *source, uint32_t length, GLint type)
+{
+    GLint status;
+    GLint shader = glCreateShader(type);
+
+    const char *src_bytes = source;
+    const GLint len = length;
+    glShaderSource(shader, 1, &src_bytes, &len);
+    glCompileShader(shader);
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (status == 0)
+    {
+        GLint log_length;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_length);
+        char *info = (char*)malloc(log_length + 1);
+        glGetShaderInfoLog(shader, log_length, NULL, info);
+        fprintf(stderr, "Error: failed to compile shader:\n%s\n", info);
+        free(info);
+
+        return -1;
+    }
+
+    return shader;
+}
+
+void CreateShaderProgram(GLint vertex_shader, GLint fragment_shader, GLuint *program)
+{
+    *program = glCreateProgram();
+    glAttachShader(*program, vertex_shader);
+    glAttachShader(*program, fragment_shader);
+}
+
+void LinkShaderProgram(GLuint program)
+{
+    GLint status;
+    glLinkProgram(program);
+
+    glGetProgramiv(program, GL_LINK_STATUS, &status);
+    if (status == 0)
+    {
+        fprintf(stderr, "Error: unable to initialize shader program\n");
+    }
+}
+
+int32_t ReadFile(const char* filename, char** data_ptr)
+{
+    FILE *fp = fopen(filename, "rb");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "Error: cannot open %s\n", filename);
+        return -1;
+    }
+
+    fseek(fp, 0, SEEK_END);
+    int32_t fsize = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+
+    *data_ptr = (char*)malloc(fsize);
+    size_t read = fread(*data_ptr, fsize, 1, fp);
+    if (read != 1)
+    {
+        fprintf(stderr, "Error cannot read %s\n", filename);
+        return -1;
+    }
+
+    fclose(fp);
+
+    return fsize;
 }
--- src/server/pxserver.cpp
+++ src/server/pxserver.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <netsocket/server.h>
+#include <ifaddrs.h>
 #include <mpi.h>
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -17,24 +18,16 @@ typedef struct ImageData {
     int32_t height;
 } ImageData;
 
-void OnConnection(NetSocket::Server& server, NetSocket::ClientConnection::Pointer client);
-void OnDisonnect(NetSocket::Server& server, std::string client_endpoint);
-void OnStringMessage(NetSocket::Server& server, NetSocket::ClientConnection::Pointer client, std::string message);
+void GetIpAddress(const char *iface, uint8_t ip_address[4]);
 void GetFrameList(std::string img_template, int start, int increment, std::vector<std::string> *frame_list);
-void GetImageReadIndices(int num_images);
-int FindImageOwner(int idx);
+void GetImageReadIndices(int num_images, int num_ranks, int *img_start_idx, int *img_end_idx);
+int FindImageOwner(int idx, int num_ranks, int *img_start_idx, int *img_end_idx);
 int32_t ReadFile(const char *filename, char **data);
-
-std::vector<CompressedImageData> compressed_images;
-ImageData send_img;
-int rank, num_ranks, img_idx;
-int *img_start_idx, *img_end_idx, *scatter_counts, *scatter_offsets;
-unsigned char *scatter_recv;
 
 int main(int argc, char **argv)
 {
     // initialize MPI
-    int rc;
+    int rc, rank, num_ranks;
     rc = MPI_Init(&argc, &argv);
     rc |= MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     rc |= MPI_Comm_size(MPI_COMM_WORLD, &num_ranks);
@@ -56,56 +49,174 @@ int main(int argc, char **argv)
     if (argc >= 4) increment = atoi(argv[3]);
     std::vector<std::string> frame_list;
     GetFrameList(argv[1], start, increment, &frame_list);
+    if (rank == 0) printf("Found %d images to stream\n", frame_list.size());
 
-    img_start_idx = new int[num_ranks];
-    img_end_idx = new int[num_ranks];
-    GetImageReadIndices(frame_list.size());
-    printf("Rank %d: start %d, end %d\n", rank, img_start_idx[rank], img_end_idx[rank]);
+    int *img_start_idx = new int[num_ranks];
+    int *img_end_idx = new int[num_ranks];
+    GetImageReadIndices(frame_list.size(), num_ranks, img_start_idx, img_end_idx);
 
     // read images into memory
-    img_idx = 0;
     int i, w, h, c;
     char *img_data;
     int32_t img_len;
+    std::vector<CompressedImageData> compressed_images;
     for (i = img_start_idx[rank]; i <= img_end_idx[rank]; i++)
     {
         img_len = ReadFile(frame_list[i].c_str(), &img_data);
         compressed_images.push_back({reinterpret_cast<unsigned char*>(img_data), img_len});
     }
+    ImageData send_img;
     send_img.data = stbi_load_from_memory(compressed_images[0].data, compressed_images[0].length, &w, &h, &c, STBI_rgb_alpha);
     send_img.width = w;
     send_img.height = h;
+    if (rank == 0) printf("Image load complete (%dx%d)\n", w, h);
     int rows_per_rank = send_img.height / num_ranks;
     int rows_extra = send_img.height % num_ranks;
-    scatter_counts = new int[num_ranks];
-    scatter_offsets = new int[num_ranks];
+    int *scatter_counts = new int[num_ranks];
+    int *scatter_offsets = new int[num_ranks];
     int offset = 0;
     for (i = 0; i < num_ranks; i++)
     {
         scatter_offsets[i] = offset;
-        offset += rows_per_rank * 4;
+        offset += send_img.width * rows_per_rank * 4;
         if (i < rows_extra)
         {
-            offset++;
+            offset += send_img.width * 4;
         }
         scatter_counts[i] = offset - scatter_offsets[i];
     }
-    scatter_recv = new unsigned char[scatter_counts[rank]];
+    unsigned char *scatter_recv = new unsigned char[scatter_counts[rank]];
+
+    // gather ip address and port info
+    uint8_t ip_address[4];
+    GetIpAddress("en0", ip_address);
+    uint16_t port = 8000 + rank;
+    uint16_t net_port = htons(port);
+    uint8_t *ip_address_list = NULL;
+    uint16_t *port_list = NULL;
+    if (rank == 0)
+    {
+        ip_address_list = new uint8_t[4 * num_ranks];
+        port_list = new uint16_t[num_ranks];
+    }
+    MPI_Gather(ip_address, 4, MPI_UNSIGNED_CHAR, ip_address_list, 4, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
+    MPI_Gather(&net_port, 1, MPI_UNSIGNED_SHORT, port_list, 1, MPI_UNSIGNED_SHORT, 0, MPI_COMM_WORLD);
 
     // create NetSocket server (1 per rank)
-    uint16_t port = 8000 + rank;
+    if (rank == 0) printf("Waiting for clients to connect\n");
     NetSocket::ServerOptions options = NetSocket::CreateServerOptions();
     options.flags = NetSocket::GeneralFlags::None;
     NetSocket::Server server(port, options);
-    server.ConnectCallback(OnConnection);
-    server.DisconnectCallback(OnDisonnect);
-    server.Run();
+
+    // wait for all clients to connect
+    int num_remote_ranks;
+    NetSocket::ClientConnection::Pointer client = NULL;
+    if (rank == 0)
+    {
+        bool client_info_received = false;
+        while (!client_info_received) {
+            NetSocket::Server::Event event = server.WaitForNextEvent();
+            switch (event.type)
+            {
+                case NetSocket::Server::EventType::Connect:
+                    client = event.client;
+                    event.client->Send(ip_address_list, 4 * num_ranks, NetSocket::CopyMode::ZeroCopy);
+                    event.client->Send(port_list, num_ranks * sizeof(uint16_t), NetSocket::CopyMode::ZeroCopy);
+                    break;
+                case NetSocket::Server::EventType::Disconnect:
+                    std::cout << "Error: Client disconnected" << std::endl;
+                    break;
+                case NetSocket::Server::EventType::ReceiveBinary:
+                    if (event.data_length == 9 && ((uint8_t*)event.binary_data)[0] == 1 && ntohl(*((uint32_t*)((uint8_t*)event.binary_data + 1))) == num_ranks)
+                    {
+                        num_remote_ranks = ntohl(*((uint32_t*)((uint8_t*)event.binary_data + 5)));
+                        client_info_received = true;
+                    }
+                    else
+                    {
+                        std::cout << "Error: Unexpected client response" << std::endl;
+                    }
+                    delete[] event.binary_data;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    MPI_Bcast(&num_remote_ranks, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    if (rank == 0) printf("Discovered %d remote clients\nBegin establishing connections\n", num_remote_ranks);
+
+    while (client == NULL)
+    {
+        NetSocket::Server::Event event = server.WaitForNextEvent();
+        if (event.type == NetSocket::Server::EventType::Connect)
+        {
+            client = event.client;
+        }
+    }
+    uint32_t size[2];
+    size[0] = htonl(send_img.width);
+    size[1] = htonl(scatter_counts[rank] / (4 * send_img.width));
+    client->Send(size, 2 * sizeof(uint32_t), NetSocket::CopyMode::MemCopy);
+    NetSocket::Server::Event event;
+    do
+    {
+        event = server.WaitForNextEvent();
+    } while (event.type != NetSocket::Server::EventType::SendFinished);
+    printf("[rank %d] client connected and data size sent\n", rank);
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (rank == 0) printf("All clients connected!\n");
+
+    // stream loop
+    if (rank == 0) printf("Begin stream loop\n");
+    for (i = 0; i <frame_list.size(); i++)
+    {
+        printf("[rank %d] starting iteration %d\n", rank, i);
+        if (i >= img_start_idx[rank] && i <= img_end_idx[rank])
+        {
+            MPI_Scatterv(send_img.data, scatter_counts, scatter_offsets, MPI_UNSIGNED_CHAR, scatter_recv, scatter_counts[rank], MPI_UNSIGNED_CHAR, rank, MPI_COMM_WORLD);
+            client->Send(scatter_recv, scatter_counts[rank], NetSocket::CopyMode::ZeroCopy);
+            free(send_img.data);
+            if (i + 1 < compressed_images.size())
+            {
+                int w, h, c;
+                int compress_idx = i + 1 - img_start_idx[rank];
+                send_img.data = stbi_load_from_memory(compressed_images[compress_idx].data, compressed_images[compress_idx].length, &w, &h, &c, STBI_rgb_alpha);
+                send_img.width = w;
+                send_img.height = h;
+            }
+            NetSocket::Server::Event event = server.WaitForNextEvent();
+            if (event.type != NetSocket::Server::EventType::SendFinished)
+            {
+                std::cout << "Error: Unexpected event " << event.type << std::endl;
+            }
+        }
+        else
+        {
+            int root = FindImageOwner(i, num_ranks, img_start_idx, img_end_idx);
+            MPI_Scatterv(NULL, scatter_counts, scatter_offsets, MPI_UNSIGNED_CHAR, scatter_recv, scatter_counts[rank], MPI_UNSIGNED_CHAR, root, MPI_COMM_WORLD);
+            client->Send(scatter_recv, scatter_counts[rank], NetSocket::CopyMode::ZeroCopy);
+            NetSocket::Server::Event event = server.WaitForNextEvent();
+            if (event.type != NetSocket::Server::EventType::SendFinished)
+            {
+                std::cout << "Error: Unexpected event " << event.type << std::endl;
+            }
+        }
+
+        NetSocket::Server::Event event;
+        do
+        {
+            event = server.WaitForNextEvent();
+        } while (!(event.type == NetSocket::Server::EventType::ReceiveBinary && event.data_length == 1 && ((uint8_t*)event.binary_data)[0] == 1));
+    }
+    if (rank == 0) printf("all done - goodbye\n");
+
 
     MPI_Finalize();
     
     return 0;
 }
-
+/*
 void OnConnection(NetSocket::Server& server, NetSocket::ClientConnection::Pointer client)
 {
     client->ReceiveStringCallback(OnStringMessage);
@@ -182,6 +293,30 @@ void OnStringMessage(NetSocket::Server& server, NetSocket::ClientConnection::Poi
         }
     }
 }
+*/
+
+void GetIpAddress(const char *iface, uint8_t ip_address[4])
+{
+    struct ifaddrs *interfaces = NULL;
+    int success = getifaddrs(&interfaces);
+    if (success == 0)
+    {
+        struct ifaddrs *temp_addr = interfaces;
+        while (temp_addr != NULL)
+        {
+            if(temp_addr->ifa_addr->sa_family == AF_INET)
+            {
+                if (strcmp(temp_addr->ifa_name, iface) == 0)
+                {
+                    memcpy(ip_address, &(((struct sockaddr_in*)temp_addr->ifa_addr)->sin_addr), 4);
+                    break;
+                }
+            }
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    freeifaddrs(interfaces);
+}
 
 void GetFrameList(std::string img_template, int start, int increment, std::vector<std::string> *frame_list)
 {
@@ -255,7 +390,7 @@ void GetFrameList(std::string img_template, int start, int increment, std::vecto
     frame_list->pop_back();
 }
 
-void GetImageReadIndices(int num_images)
+void GetImageReadIndices(int num_images, int num_ranks, int *img_start_idx, int *img_end_idx)
 {
     int i;
     int img_per_rank = num_images / num_ranks;
@@ -273,7 +408,7 @@ void GetImageReadIndices(int num_images)
     }
 }
 
-int FindImageOwner(int idx)
+int FindImageOwner(int idx, int num_ranks, int *img_start_idx, int *img_end_idx)
 {
     int i;
     for (i = 0; i < num_ranks; i++)
